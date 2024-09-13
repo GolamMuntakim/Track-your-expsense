1e766a5368e9dacbef61e7398b84e057f11bc31c
Golam Muntakim <seams123456789@gmail.com>
2024-09-13 11:06:48 -0700

    project created

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+
+// import ContextMenu from './Components/ContextMenu'
+import ExpenseForm from './Components/ExpenseForm'
+import ExpenseTable from './Components/ExpenseTable'
+import './App.css'
+import { useState } from 'react'
+import ExpenseData from './ExpenseData'
+
+function App() {
+  const [expenses, setExpenses] = useState(ExpenseData)
+  console.log(expenses)
+
+  return (
+    <>
+      <main>
+        <h1>Track Your Expense</h1>
+        <div className="expense-tracker">
+          <ExpenseForm></ExpenseForm>
+          <ExpenseTable></ExpenseTable>
+          {/* <ContextMenu></ContextMenu> */}
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
