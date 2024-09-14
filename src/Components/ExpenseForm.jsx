03d2c6a845b067ab3d5c5ce6e321c47ec4ff92c0
Golam Muntakim <seams123456789@gmail.com>
2024-09-13 21:00:09 -0700

    form validation done

--- src/Components/ExpenseForm.jsx
+++ src/Components/ExpenseForm.jsx
@@ -1,55 +1,92 @@
-import { useState } from "react";
+import  { useState } from 'react'
 
-
-const ExpenseForm = ({setExpenses}) => {
-    const[expense , setExpense] = useState({
-        title: '',
-        category: '',
-        amount: ''
-    })
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-        console.log(expense)
-        setExpenses((prevState) => [...prevState, {...expense, id:crypto.randomUUID()}])
-      setExpense({
-        title: '',
-        category: '',
-        amount: ''
-      })
+export default function ExpenseForm({ setExpenses }) {
+  const [expense, setExpense] = useState({
+    title: '',
+    category: '',
+    amount: '',
+  })
+  const [errors, setErrors] = useState({})
+  const validate = (formData) =>{
+    const errorsData = {}
+    if(!formData.title){
+        errorsData.title = "Title is required"
     }
-    
-   
-    return (
-        <div>
-              <form className="expense-form" onSubmit={handleSubmit}>
+    if(!formData.category){
+        errorsData.category = 'Category is required'
+    }
+    if(!formData.amount){
+        errorsData.amount = 'Amount is required'
+    }
+    setErrors(errorsData)
+    return errorsData
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const validateResullt = validate(expense)
+    if(Object.keys(validateResullt).length) return
+    setExpenses((prevState) => [
+      ...prevState,
+      { ...expense, id: crypto.randomUUID() },
+    ])
+    setExpense({
+      title: '',
+      category: '',
+      amount: '',
+    })
+  }
+
+  console.log(expense)
+
+  const handleChange = (e) => {
+        const {name, value} = e.target
+        setExpense(
+            (prevState) => ({ ...prevState, [name]:value }))
+            setErrors({})
+  }
+
+  return (
+    <form className="expense-form" onSubmit={handleSubmit}>
       <div className="input-container">
         <label htmlFor="title">Title</label>
-        <input id="title" name="title" value={expense.title} 
-        onChange={(e)=> setExpense((prevState)=>({...prevState, title: e.target.value}))} />
+        <input
+          id="title"
+          name="title"
+          value={expense.title}
+          onChange={handleChange}
+        />
+        <p>{errors.title}</p>
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
-        <select id="category" name="category" value={expense.category}
-        onChange={(e)=> setExpense((prevState)=>({...prevState, category: e.target.value}))}>
+        <select
+          id="category"
+          name="category"
+          value={expense.category}
+          onChange={handleChange}
+        >
           <option value="" hidden>
             Select Category
           </option>
-          <option value="grocery">Grocery</option>
-          <option value="clothes">Clothes</option>
-          <option value="bills">Bills</option>
-          <option value="education">Education</option>
-          <option value="medicine">Medicine</option>
+          <option value="Grocery">Grocery</option>
+          <option value="Clothes">Clothes</option>
+          <option value="Bills">Bills</option>
+          <option value="Education">Education</option>
+          <option value="Medicine">Medicine</option>
         </select>
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
-        <input id="amount" name="amount" value={expense.amount}
-        onChange={(e)=> setExpense((prevState)=>({...prevState, amount: e.target.value}))}/>
+        <input
+          id="amount"
+          name="amount"
+          value={expense.amount}
+          onChange={handleChange
+          }
+        />
       </div>
       <button className="add-btn">Add</button>
     </form>
-        </div>
-    );
-};
-
-export default ExpenseForm;
+  )
+}
