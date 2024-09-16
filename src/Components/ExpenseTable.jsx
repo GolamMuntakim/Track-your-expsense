6948ed86e49af7bf568159d4cfb1c80a76e5d1ef
Golam Muntakim <seams123456789@gmail.com>
2024-09-15 22:20:11 -0700

    sorting functionality done

--- src/Components/ExpenseTable.jsx
+++ src/Components/ExpenseTable.jsx
@@ -11,6 +11,8 @@ const ExpenseTable = ({expenses, setExpenses, setExpense, setEditingRowId}) => {
   const total =  result.reduce((accumulator, current)=> accumulator + parseInt(current.amount) , 0)
   console.log(total)
   const [rowId, setRowId] = useState('')
+  const [sortCallback , setSortCallback] = useState(()=> ()=>{})
+  console.log(sortCallback)
 
 
     return (
@@ -18,10 +20,44 @@ const ExpenseTable = ({expenses, setExpenses, setExpense, setEditingRowId}) => {
         <ContextMenu menuPosition={menuPosition} setMenuPosition={setMenuPosition} 
         setExpenses={setExpenses} rowId={rowId} setExpense={setExpense} expenses={expenses} 
         setEditingRowId={setEditingRowId} ></ContextMenu>
-             <table className="expense-table" onClick={(e)=> setMenuPosition({})}>
+             <table className="expense-table" onClick={()=>{
+              if(menuPosition.left){
+                setMenuPosition({})
+              }
+             }}>
           <thead>
             <tr>
-              <th>Title</th>
+              <th className="amount-column"> <div>
+                  <span>Title</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="10"
+                    viewBox="0 0 384 512"
+                    className="arrow up-arrow"
+                    onClick={()=>{
+                      setSortCallback(()=>(a,b)=> a.title.localeCompare(b.title))
+                    }}
+                  >
+                    <title>Ascending</title>
+                    <path
+                      d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"
+                    />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="10"
+                    viewBox="0 0 384 512"
+                    className="arrow down-arrow"
+                    onClick={()=>{
+                      setSortCallback(()=>(a,b)=> b.title.localeCompare(a.title))
+                    }}
+                  >
+                    <title>Descending</title>
+                    <path
+                      d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"
+                    />
+                  </svg>
+                </div></th>
               <th>
                 <select onChange={(e)=>setQuery(e.target.value.toLowerCase())}>
                   <option value="">All</option>
@@ -40,6 +76,9 @@ const ExpenseTable = ({expenses, setExpenses, setExpense, setEditingRowId}) => {
                     width="10"
                     viewBox="0 0 384 512"
                     className="arrow up-arrow"
+                    onClick={()=>{
+                      setSortCallback(()=>(a,b)=> a.amount - b.amount)
+                    }}
                   >
                     <title>Ascending</title>
                     <path
@@ -51,6 +90,9 @@ const ExpenseTable = ({expenses, setExpenses, setExpense, setEditingRowId}) => {
                     width="10"
                     viewBox="0 0 384 512"
                     className="arrow down-arrow"
+                    onClick={()=>{
+                      setSortCallback(()=>(a,b)=>  b.amount - a.amount)
+                    }}
                   >
                     <title>Descending</title>
                     <path
@@ -63,7 +105,7 @@ const ExpenseTable = ({expenses, setExpenses, setExpense, setEditingRowId}) => {
           </thead>
           <tbody>
             {
-                result.map((expenses)=>(
+                result.sort(sortCallback).map((expenses)=>(
                     <tr key={expenses?.id}   
                     onContextMenu={(e)=>{
                       e.preventDefault()
@@ -79,7 +121,9 @@ const ExpenseTable = ({expenses, setExpenses, setExpense, setEditingRowId}) => {
             
             <tr>
               <th>Total</th>
-              <th></th>
+              <th className="clear-sort" onClick={()=>{
+                setSortCallback(()=>()=>{})
+              }}>Clear Sorting</th>
               <th>₹{total}</th>
             </tr>
           </tbody>
